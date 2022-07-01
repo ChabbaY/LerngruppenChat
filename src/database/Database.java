197a2a033bd253fcfe32035350418f1cb9eea6ae
ChabbaY_Elitebook <linus.englert@siemens.com>
2022-07-01 23:17:39 +0200

    basic data base usage insert into data base: name and frage if not already exist, mitglied frage if not already exist else update

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,4 +1,140 @@
 package database;
 
+import java.sql.*;
+
 public class Database {
+    private final String USER, PASS;
+    private final Connection conn;
+
+    public Database(String user, String pass) {
+        USER = user;
+        PASS = pass;
+        conn = getConnection();
+    }
+
+    private Connection getConnection() {
+        try {
+            String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+            Class.forName(JDBC_DRIVER);
+            String DB_URL = "jdbc:mariadb://chabbay.de/";
+            return DriverManager.getConnection(DB_URL + "lerngruppe", USER, PASS);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Treiber nicht gefunden");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Fehler bei Verbindungsaufbau");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private int getMitgliedID(String name) {
+        if (conn == null) return -1;
+        String sql = "SELECT id FROM mitglied WHERE name=?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            ResultSet result = pstmt.executeQuery();
+            while(result.next()) {
+                return pstmt.getResultSet().getInt("id");
+            }
+        } catch (SQLException e) {
+            System.out.println("Datenbankfehler");
+            e.printStackTrace();
+        }
+        return -1;
+    }
+    private int getFrageID(String frage) {
+        if (conn == null) return -1;
+        String sql = "SELECT id FROM frage WHERE frage=?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, frage);
+            ResultSet result = pstmt.executeQuery();
+            while(result.next()) {
+                return pstmt.getResultSet().getInt("id");
+            }
+        } catch (SQLException e) {
+            System.out.println("Datenbankfehler");
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    private void setMitglied(String name) {
+        if (conn == null) return;
+        String sql = "INSERT INTO mitglied (name) VALUES (?)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Datenbankfehler");
+            e.printStackTrace();
+        }
+    }
+    private void setFrage(String frage) {
+        if (conn == null) return;
+        String sql = "INSERT INTO frage (frage) VALUES (?)";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, frage);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Datenbankfehler");
+            e.printStackTrace();
+        }
+    }
+
+    public void setDetails(String mitglied, String frage, int frage_nr, String antwort) {
+        if (conn == null) return;
+        boolean has_entry = false;
+        //insert mitglied if not exists and get id
+        if (getMitgliedID(mitglied) == -1) setMitglied(mitglied);
+        int mitglied_id = getMitgliedID(mitglied);
+
+        //insert frage if not exists and get id
+        if (getFrageID(frage) == -1) setFrage(frage);
+        int frage_id = getFrageID(frage);
+
+        //insert or update data
+        String sql = "SELECT * FROM mitglied_frage WHERE mitglied_id=? AND frage_id=?";
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, mitglied_id);
+            pstmt.setInt(2, frage_id);
+            ResultSet result = pstmt.executeQuery();
+            while(result.next()) {
+                has_entry = true;
+            }
+        } catch (SQLException e) {
+            System.out.println("Datenbankfehler");
+            e.printStackTrace();
+        }
+        if (!has_entry) {//Mitglied hat Frage noch nicht beantwortet
+            sql = "INSERT INTO mitglied_frage (mitglied_id, frage_id, frage_nr, antwort) VALUES (?, ?, ?, ?)";
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setInt(1, mitglied_id);
+                pstmt.setInt(2, frage_id);
+                pstmt.setInt(3, frage_nr);
+                pstmt.setString(4, antwort);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println("Datenbankfehler");
+                e.printStackTrace();
+            }
+        } else {
+            sql = "UPDATE mitglied_frage SET frage_nr=?, antwort=?";
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setInt(1, frage_nr);
+                pstmt.setString(2, antwort);
+                pstmt.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println("Datenbankfehler");
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/gui/Window.java
+++ src/gui/Window.java
@@ -1,4 +1,88 @@
 package gui;
 
-public class Window {
+import database.Database;
+
+import javax.swing.*;
+import javax.swing.text.NumberFormatter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.NumberFormat;
+
+public class Window extends JFrame {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private static Database database;
+
+    JTextField name_input, frage_input, antwort_input;
+    JFormattedTextField frage_nr_input;
+    JButton commit_frage;
+
+    public Window() {
+        super("Lerngruppe - Chat");
+        this.setSize(1000, 1000);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        this.add(panel);
+
+        NumberFormat format = NumberFormat.getInstance();
+        format.setGroupingUsed(false);
+        NumberFormatter formatter = new NumberFormatter(format);
+        formatter.setAllowsInvalid(false);
+        initializeDBConn();
+
+        panel.add(new JLabel("Name"));
+        name_input = new JTextField();
+        panel.add(name_input);
+        panel.add(new JLabel("Neue Frage"));
+        frage_input = new JTextField();
+        panel.add(frage_input);
+        panel.add(new JLabel("Frage Nr."));
+        frage_nr_input = new JFormattedTextField(formatter);
+        panel.add(frage_nr_input);
+        panel.add(new JLabel("Antwort"));
+        antwort_input = new JTextField();
+        panel.add(antwort_input);
+        commit_frage = new JButton("absenden");
+        panel.add(commit_frage);
+        commit_frage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = name_input.getText().trim();
+                String frage = frage_input.getText().trim();
+                int frage_nr = Integer.parseInt(frage_nr_input.getText());
+                String antwort = antwort_input.getText().trim();
+
+                if (name.length() > 64) {
+                    System.out.println("name zu lang (64)");
+                    return;
+                }
+                if (antwort.length() > 2) {
+                    System.out.println("antwort zu lang (2)");
+                    return;
+                }
+
+                database.setDetails(name, frage, frage_nr, antwort);
+            }
+        });
+
+        this.setVisible(true);
+    }
+
+    public static void initializeDBConn() {
+        String user = null, pass = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("res/credentials.txt"))) {
+            user = br.readLine();
+            pass = br.readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("Datei nicht gefunden");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei");
+            e.printStackTrace();
+        }
+        database = new Database(user, pass);
+    }
 }
